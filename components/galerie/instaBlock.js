509720f19ce0ae46f0f37ddded7df768581cb658
Alexandru Codreanu <alexandru.codreanu.04@gmail.com>
2021-06-26 00:54:56 +0300

    New instagram approach

--- components/galerie/instaBlock.js
+++ components/galerie/instaBlock.js
@@ -1,12 +1,34 @@
+import { useEffect } from 'react';
+
+const useScript = url => {
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = url;
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, [url]);
+};
+
 export default function InstaBlock () {
 
+    useScript("/instafeed.min.js")
+    useScript("/instaToken.js")
+
     return (
-        <div className="w-full h-auto px-container-sm md:px-container-md lg:px-container-lg xl:px-container-xl pt-140px pb-224px font-Ubuntu bg-ui-dark">
+        <div className="w-full h-auto px-container-sm md:px-container-md lg:px-container-lg xl:px-container-xl pt-140px pb-224px font-Ubuntu bg-ui-dark text-ui-white">
             <div id="instafeed"></div>
 
-            <script src="./instaToken.js"></script>
+            Alo
 
-            <script src="./instafeed.min.js"></script>
+            {/* <script src="./instaToken.js"></script> */}
+
+            {/* <script src="./instafeed.min.js"></script> */}
 
             {/* <script src="./instaFeed.js"></script> */}
         </div>
--- pages/galerie.js
+++ pages/galerie.js
@@ -6,6 +6,7 @@ import { useContext } from "react";
 import { DeviceTypeContext } from "../components/context";
 import { API_URL } from "../utils/urls";
 import InstaBlock from "../components/galerie/instaBlock";
+import Head from "next/head";
 
 export default function Galerie({products}){
 
@@ -14,7 +15,15 @@ export default function Galerie({products}){
 
     return (
         <Layout>
-            <InstaBlock></InstaBlock>
+            {/* <InstaBlock></InstaBlock> */}
+            <Head>
+              <script type="text/javascript" src="/static/instafeed.min.js"></script>
+              <script type="text/javascript" src="/static/instaToken.js"></script>
+              {/* <script type="text/javascript" dangerouslySetInnerHTML={{ __html : process.env.NEXT_PUBLIC_rawInstaFeedMin}}></script>
+              <script type="text/javascript" dangerouslySetInnerHTML={{ __html : process.env.NEXT_PUBLIC_rawInstaToken}}></script> */}
+            </Head>
+
+            <div id="instafeed"></div>
             <GalerieComponent 
               deviceType={deviceType}
               products={products}
