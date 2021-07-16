cfd9c5744bcb49c5f9d1b3b950ab33f87bf89053
Alexandru Codreanu <alexandru.codreanu.04@gmail.com>
2021-07-16 16:34:53 +0300

    New callback

--- pages/api/payment/paynet/callback.js
+++ pages/api/payment/paynet/callback.js
@@ -1,65 +1,51 @@
 export default async (req, res) => {
-    if (req.method !== 'POST') {
-      res.status(400).send({ message: 'Only POST requests allowed' })
-      return
+  if (req.method !== 'POST') {
+    res.status(400).send({ message: 'Only POST requests allowed' })
+    return
+  }
+
+  const body = req.body
+  console.log(body)
+  console.log("body.EventType == 'Paid' ", body.EventType == 'PAID')
+
+  if (body.EventType == 'PAID') {
+    console.log("Trying fetch")
+    console.log("External ID : ", body.Payment.ExternalId)
+
+    const requestOptions = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        status_plata: "platit"
+      })
     }
-  
-    const body = req.body
-    console.log(body)
-    console.log("body.EventType == 'Paid' ",body.EventType == 'PAID')
+    // fetch(`https://mirrors-md-admin.herokuapp.com/clients/${body.Payment.ExternalId}`, 
+    const response = await fetch(`https://mirrors-md-admin.herokuapp.com/clients/${body.Payment.ExternalId}`, requestOptions)
+    console.log("Response from fetch is: ", await response.json())
 
-    if(body.EventType == 'PAID'){
-        console.log("Trying fetch")
-        console.log("External ID : ", body.Payment.ExternalId)
-
-        const requestOptions = {
-          method: 'PUT',
-          headers: { 
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            status_plata : "platit"
-          })
+    try {
+      res.status(200).json({
+        Eventid: body.Eventid,
+        EventType: body.EventType,
+        EventDate: body.EventDate,
+        Payment: {
+          Id: body.Payment.Id,
+          ExternalId: body.Payment.ExternalId,
+          Merchant: body.Payment.Merchant,
+          Customer: body.Payment.Customer,
+          StatusDate: body.Payment.StatusDate,
+          Amount: body.Payment.Amount
         }
-        // fetch(`https://mirrors-md-admin.herokuapp.com/clients/${body.Payment.ExternalId}`, 
-        fetch(`https://mirrors-md-admin.herokuapp.com/clients/783770178653`, 
-          {
-            method: 'PUT',
-            headers: { 
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              status_plata : "platit"
-            })
-          }
-        )
-        .then(response => response.json())
-        .then(
-          data => console.log(data)
-        )
-        .catch(error => console.log("Error with fetch request ", error))
-
-      try {
-          res.status(200).json({
-            Eventid : body.Eventid,
-            EventType : body.EventType,
-            EventDate : body.EventDate,
-            Payment : {
-                Id : body.Payment.Id,
-                ExternalId : body.Payment.ExternalId,
-                Merchant : body.Payment.Merchant,
-                Customer : body.Payment.Customer,
-                StatusDate : body.Payment.StatusDate,
-                Amount : body.Payment.Amount
-            }
-          })
-      } catch (error) {
-        console.log('Error inside PAID EventType')
-        res.status(500).json({ error: error })
-      }
+      })
+    } catch (error) {
+      console.log('Error inside PAID EventType')
+      res.status(500).json({ error: error })
     }
-    else{
-      console.log('Error outside PAID EventType')
-      res.status(400).json({error: "Wrong request"})
-    }
+  }
+  else {
+    console.log('Error outside PAID EventType')
+    res.status(400).json({ error: "Wrong request" })
+  }
 }
