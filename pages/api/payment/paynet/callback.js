0b9cf323116882f95261840d2b394dcd17572446
Alexandru Codreanu <alexandru.codreanu.04@gmail.com>
2021-07-16 13:16:20 +0300

    Testing callback again

--- pages/api/payment/paynet/callback.js
+++ pages/api/payment/paynet/callback.js
@@ -6,19 +6,23 @@ export default async (req, res) => {
   
     const body = req.body
     console.log(body)
+    console.log("body.EventType == 'Paid' ",body.EventType == 'PAID')
 
-    // if(body.EventType == 'PAID'){
-      // fetch(`https://mirrors-md-admin.herokuapp.com/clients/${body.Payment.id}`, 
-      //   {
-      //     method: 'PUT',
-      //     headers: { 
-      //       'Content-Type': 'application/json',
-      //     },
-      //     body: JSON.stringify({
-      //       status_plata : "platit"
-      //     })
-      //   }
-      // )
+    if(body.EventType == 'PAID'){
+      fetch(`https://mirrors-md-admin.herokuapp.com/clients/${body.Payment.id}`, 
+        {
+          method: 'PUT',
+          headers: { 
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            status_plata : "platit"
+          })
+        }
+      )
+      .then(response => response.json())
+      .then(data => console.log(data))
+      
       try {
           res.status(200).json({
             Eventid : body.Eventid,
@@ -34,10 +38,12 @@ export default async (req, res) => {
             }
           })
       } catch (error) {
+        console.log('Error inside PAID EventType')
         res.status(500).json({ error: error })
       }
-    // }
-    // else{
-    //   res.status(400).json({error: "Wrong request"})
-    // }
+    }
+    else{
+      console.log('Error outside PAID EventType')
+      res.status(400).json({error: "Wrong request"})
+    }
 }
