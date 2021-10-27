20dd90053e09dcce073f0a6b12a993f05e416981
Alexandru Codreanu <alexandru.codreanu.04@gmail.com>
2021-10-27 23:29:24 +0300

    eliminated underscores

--- pages/api/owner.js
+++ pages/api/owner.js
@@ -5,7 +5,9 @@ sgMail.setApiKey(process.env.NEXT_PUBLIC_EMAIL_API_KEY);
 export default async (req, res) => {
   const { name, phone, address, email, pret, orders, comentariu, mod_de_plata, mod_de_livrare, created_at, country } = req.body
   
-  let created_at_new = created_at.slice(0, created_at.length-5).replace("T", " ")
+  const created_at_new = created_at.slice(0, created_at.length-5).replace("T", " ")
+  const mod_de_plata_new = mod_de_plata.replace("_", " ")
+  const mod_de_livrare_new = mod_de_livrare.replace("_", " ")
 
   const msg = {
     from: '<manager.mirrors.md@gmail.com',
@@ -19,8 +21,8 @@ export default async (req, res) => {
           email : email,
           pret : pret,
           comentariu : comentariu,
-          mod_de_plata : mod_de_plata,
-          mod_de_livrare : mod_de_livrare,
+          mod_de_plata : mod_de_plata_new,
+          mod_de_livrare : mod_de_livrare_new,
           orders : orders,
           created_at : created_at_new,
           country: country
