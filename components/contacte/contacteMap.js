ecebbf936fe34d8a154c5e9d159765af341a0ffb
Alexandru Codreanu <alexandru.codreanu.04@gmail.com>
2021-12-14 12:35:19 +0200

    fixed contacts page bug

--- components/contacte/contacteMap.js
+++ components/contacte/contacteMap.js
@@ -122,7 +122,7 @@ export default function ContacteMap ({lang}) {
                     </div>
 
                     <div className="w-full text-lg-p text-type-manatee font-normal mt-40px md:mt-0">
-                        <a href={roDomain ? "tel:40723877823" : "tel:37369482034"} target="blank">
+                        <a href={roDomain ? "tel:+40723877823" : "tel:+37369482034"} target="blank">
                             <div className="flex flex-row items-center justify-start mb-8 group">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 group-hover:text-accent-accent transition duration-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 3h5m0 0v5m0-5l-6 6M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
@@ -144,18 +144,18 @@ export default function ContacteMap ({lang}) {
                                 <span className="group-hover:text-accent-accent transition duration-300">
                                   {   
                                     roDomain ?
-                                      props.lang == "ro" ?
+                                      lang == "ro" ?
                                       "Iași, str. Morilor 11"
                                       :
-                                      props.lang == "ru" ?
+                                      lang == "ru" ?
                                       "Яссы, улица Морилор, 11"
                                       :
                                       "Iași, Morilor 11 street"
                                     :
-                                      props.lang == "ro" ?
+                                      lang == "ro" ?
                                       "str. Ismail 98"
                                       :
-                                      props.lang == "ru" ?
+                                      lang == "ru" ?
                                       "ул. Ismail 98"
                                       :
                                       "Ismail 98 street"
