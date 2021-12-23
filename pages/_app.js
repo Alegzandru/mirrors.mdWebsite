0d0814cb83f482ac5bb69e2beab4fa0f48859e7c
Alexandru Codreanu <alexandru.codreanu.04@gmail.com>
2021-12-24 00:05:09 +0200

    Removed inspect element functionallity

--- pages/_app.js
+++ pages/_app.js
@@ -26,6 +26,26 @@ function MyApp({ Component, pageProps }) {
 
   const [country, setCountry] = useState('unclear')
 
+  if (typeof window !== 'undefined') {
+    document.onkeydown = function(e) {
+      if(event.keyCode == 123) {
+        return false;
+      }
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+      }
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+      }
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+      }
+      if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+      }
+    }  
+  }
+
   useEffect(() => {
     localStorage.setItem('seenRecently', JSON.stringify(seenRecently))
   }, [seenRecently])
