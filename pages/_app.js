e622d86bd96d0fefecb53b310d52ac91f04d0d38
Alexandru Codreanu <alexandru.codreanu.04@gmail.com>
2022-10-20 11:57:20 +0300

    removed inspect and changed isRoDomain function

--- pages/_app.js
+++ pages/_app.js
@@ -44,30 +44,7 @@ function MyApp({ Component, pageProps }) {
     }
     withCountry()
   }, []);
-  
-  if (typeof window === 'object' && ['onemillory.ro', 'www.onemillory.md'].includes(window.location.hostname)) {
-    document.addEventListener('contextmenu', function(e) {
-      e.preventDefault();
-    })
-    document.onkeydown = function(e) {
-      if(event.keyCode == 123) {
-         return false;
-      }
-      if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-         return false;
-      }
-      if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-         return false;
-      }
-      if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-         return false;
-      }
-      if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-         return false;
-      }
-    }  
-  }
- 
+
   return (
     country === 'unclear' ? 
     <div className="w-full h-full bg-ui-white flex flex-row justify-center items-center">
